30c92f3f60a2bdab278d90728a7ea7ebfa3a5dfd
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-07-15 16:59:41 +0200

    User model created

---
+++ Back/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const mongooseUniqueValidator = require('mongoose-unique-validator'); /*Package permettant de valider l'unicité d'une clé via l'option "unique"*/
+const validator = require('validator'); /* Package permetant de valider certains champs comme la conformité du format de l'email via l'option validate*/
+
+const userSchema = mongoose.Schema(
+    {email: {type: "string", required: true, unique: true, validate: {
+        validator: validator.isEmail,
+        message: 'Adresse email invalide'
+    } },
+    password: {type: "string", required: true}}
+)
+
+userSchema.plugin(mongooseUniqueValidator); /*Application du plugin mongoose-unique-validator au schema*/
+
+module.export = mongoose.model('user', userSchema);
