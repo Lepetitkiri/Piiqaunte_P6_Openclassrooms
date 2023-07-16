0fdf076c94cab45510a81746deda2915b5ab6af3
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-07-16 15:05:38 +0200

    login controller initialization

--- Back/controllers/user.js
+++ Back/controllers/user.js
@@ -1,4 +1,5 @@
 const user = require('../models/user'); /*importation du schéma*/
+const mongoose = require('mongoose'); 
 const bcrypt = require('bcrypt'); /*importation du package de cryptage du password*/
 
 exports.createUser = (req, res, next) => {
@@ -13,7 +14,20 @@ exports.createUser = (req, res, next) => {
             /*Enregistrement de newUser dans la dataBase*/
         newUser.save() 
         .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch(error => res.status(400).json({ error }));
+        .catch(error => res.status(400).json({ error })); /*Erreur de sauvegarde*/
         })
-        .catch(error => res.status(500).json({error}));
+        .catch(error => res.status(500).json({error})); /*Erreur de traitement de la requete*/
+};
+
+
+exports.login = (req, res, next) => {
+    /*Recherche d'email correspondant dans la database*/
+    user.findOne({email: req.body.email}, {password: 0})
+    .then((user) => {
+        if (user !== null) {
+            res.status(200).json({ message: 'Utilisateur trouvé' })
+        } else {
+            res.status(404).json({ message: 'Mauvaise correspondance email/mot de passe !' }) /*Erreur de correspondance de l'email dans la database*/
+        }})
+    .catch(error => res.status(500).json({ error })) /*Erreur de traitement de la requete*/
 };
