bcd849cbcbddc78669e8cabdeb2e7f115af63c99
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-07-16 09:14:43 +0200

    CORS problems solved

--- Back/app.js
+++ Back/app.js
@@ -12,6 +12,14 @@ mongoose.connect(process.env.mongoDBURI,
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+  /* Prolèmes de CORS*/
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next()
+  });
+
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
