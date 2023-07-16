9d536140c93a660640e2327b07ded4c8ef966bf2
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-07-16 14:01:57 +0200

    Password encryption in createUser model

--- Back/controllers/user.js
+++ Back/controllers/user.js
@@ -1,12 +1,19 @@
 const user = require('../models/user'); /*importation du schéma*/
+const bcrypt = require('bcrypt'); /*importation du package de cryptage du password*/
 
 exports.createUser = (req, res, next) => {
-    /* Création d'une instance de user */
-    const newUser = new user({
-        email: req.body.email,
-        password: req.body.password
-    });
-    newUser.save() 
-    .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-    .catch(error => res.status(400).json({ error }));
+    /*Hashage du password*/
+        bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            /* Création d'une instance de user */
+            const newUser = new user({
+                email: req.body.email,
+                password: hash
+            });
+            /*Enregistrement de newUser dans la dataBase*/
+        newUser.save() 
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({error}));
 };
