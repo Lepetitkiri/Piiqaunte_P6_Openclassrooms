089d0a750cb02fa64d8810b9109b1f8f302365bd
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-07-16 14:24:11 +0200

    POST route for login created

--- Back/routes/user.js
+++ Back/routes/user.js
@@ -3,5 +3,6 @@ const router = express.Router() /*Création d'un router*/
 const userController = require('../controllers/user');
 
 router.post('/signup', userController.createUser);
+router.post('/login', userController.login);
 
 module.exports = router;
