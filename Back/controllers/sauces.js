d6349286649a4971c5cb2a435086868114af4fea
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-07-17 16:25:50 +0200

    getAllSauces controller created

--- Back/controllers/sauces.js
+++ Back/controllers/sauces.js
@@ -1,5 +1,8 @@
-//const sauces = require('../models/sauce');
+const sauces = require('../models/sauce');
 
+/* Récupération de toutes les sauces de la base de données via GET vers /api/sauces*/
 exports.getAllSauces = (req, res, next) => {
-    res.status(200).json({message: "La route get des sauces est paramétrée"})
+    sauces.find()
+    .then(res.status(200).json({sauces}))
+    .catch((error) => res.status(500).json({ error })); /*Erreur de traitement de la requete*/
 };
